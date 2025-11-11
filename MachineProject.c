2c27d28cd2aa541610c36417cf29fbae832bd49e
Eggboi <fabialaiggy@gmail.com>
2025-11-11 18:58:21 +0800

    Initial code outline
    need to add more

---
+++ MachineProject.c
@@ -0,0 +1,44 @@
+/*
+CCPROG1 Machine Project
+"A Walk in The Sequence Park"
+Student Name: Matthew Miguel S. Fabiala
+Section : S19B
+*/
+
+#include <stdio.h>
+
+void diceRoll()
+// random number generator with
+//if statement where (if x=6, roll again)
+
+void runLevelOne()
+// random number sequence generator
+// if statement to check if answer input given by player is correct
+
+void runLevelTwo()
+// random letter sequence generator
+// if statement to check if answer input given by player is correct
+
+void runLevelThree()
+// random leltter sequence generator
+// if statement to check if answer input given by player is correct
+
+int decideAI()
+// check if players 1, 2, 3, and 4 are played by either a human or an AI
+
+int numOfPlayers()
+// ask how many players will play the game and assign the needed values for each
+player
+
+int askLevel()
+// ask what difficulty the players would like to play and use the value to trigger
+the use of any "runLevel" function
+
+
+
+/* Dev Notes:
+- Will just see how to make each function work first, before I see how I can
+connect each function to work with one another
+- My brain hurts
+- God bless CCPROG1
+*/
